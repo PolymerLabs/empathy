75744e5fab523c8c1cd38f4ef487362a15031959
Chris Joel <cdata@google.com>
2018-03-22 22:26:41 -0700

    Add command line arg for output folder

--- src/empathy.ts
+++ src/empathy.ts
@@ -1,4 +1,4 @@
-import { sep, join } from 'path';
+import { sep, resolve } from 'path';
 import { promisify } from 'util';
 import * as vfs from 'vinyl-fs';
 import * as fsExtra from 'fs-extra';
@@ -7,33 +7,33 @@ import { assetStage, AssetStageConfig } from './asset-stage.js';
 
 const copy = promisify(fsExtra.copy);
 
-export const applyEmpathy = async (config: AssetStageConfig): Promise<void> => {
-  const assetStagePath = await assetStage(config);
+export const applyEmpathy =
+    async (config: AssetStageConfig, outputFolder: string): Promise<void> => {
+      const assetStagePath = await assetStage(config);
 
-  try {
-    await new Promise((resolve, reject) => {
-      vfs.src([`${assetStagePath}${sep}**${sep}*.js`], {
-        cwd: assetStagePath
-      })
-          .pipe(bareSpecifiersTransform())
-          .on('error', reject)
-          .pipe(vfs.dest(assetStagePath))
-          .on('error', reject)
-          .on('end', () => resolve())
-    });
+      try {
+        await new Promise((resolve, reject) => {
+          vfs.src([`${assetStagePath}${sep}**${sep}*.js`], {
+            cwd: assetStagePath
+          })
+              .pipe(bareSpecifiersTransform())
+              .on('error', reject)
+              .pipe(vfs.dest(assetStagePath))
+              .on('error', reject)
+              .on('end', () => resolve())
+        });
 
-    console.log('Empathy applied!');
-  } catch (error) {
-    console.error('Failed to transform asset specifiers');
-    console.error(error);
-  }
+        console.log('Empathy applied!');
+      } catch (error) {
+        console.error('Failed to transform asset specifiers');
+        console.error(error);
+      }
 
-  try {
-    await copy(assetStagePath, join(process.cwd(), 'assets'));
-  } catch (error) {
-    console.error('Failed to create assets directory');
-    console.error(error);
-  }
-};
-
+      try {
+        await copy(assetStagePath, resolve(outputFolder));
+      } catch (error) {
+        console.error('Failed to create assets directory');
+        console.error(error);
+      }
+    };
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import * as nodePath from 'path';
+import { relative, join } from 'path';
 import { applyEmpathy } from './empathy.js';
+import * as commandLineArgs from 'command-line-args';
 
-const manifestPath = nodePath.join(process.cwd(), 'package.json');
+const cwd = process.cwd();
+const optionDefinitions = [{
+  name: 'out',
+  alias: 'o',
+  type: String,
+  multiple: false,
+  defaultOption: true,
+  defaultValue: join(cwd, 'assets')
+}];
+
+const options = commandLineArgs(optionDefinitions);
+const { out } = options;
+
+const manifestPath = join(cwd, 'package.json');
 const manifest = require(manifestPath);
 const { assetDependencies } = manifest;
 
@@ -9,8 +23,8 @@ if (assetDependencies == null) {
   console.log('No asset dependencies listed in package.json.');
   process.exit(0);
 } else {
-  applyEmpathy(assetDependencies).then(() => {
-    console.log('Assets installed!');
+  applyEmpathy(assetDependencies, out).then(() => {
+    console.log(`Assets installed to "${relative(cwd, out)}" 🖖`);
   }).catch(error => {
     console.error(error);
   });
